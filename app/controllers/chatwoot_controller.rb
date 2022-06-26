ac9ee66a2c800f395319afc3613fa96f5fc1f62a
Douglas Lara <douglasdelaras@gmail.com>
2022-06-26 16:10:16 -0300

    Fix event failure response

--- app/controllers/chatwoot_controller.rb
+++ app/controllers/chatwoot_controller.rb
@@ -2,6 +2,6 @@ class ChatwootController < ApplicationController
   def webhook
     Chatwoot::ReceiveEvent.call(event: params)
     .on_success { |result| render(200, json: result.data) }
-    .on_failure { |result| render_json(500, json: result.data) }
+    .on_failure { |result| render(500, json: result.data) }
   end
 end
