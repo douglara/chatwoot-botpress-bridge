8066483946dfa67e04e333c8aad0d0129b05018d
Douglas Lara <douglasdelaras@gmail.com>
2022-06-27 18:59:15 -0300

    Create test for botpress many messages

--- test/use_cases/chatwoot/receive_event_test.rb
+++ test/use_cases/chatwoot/receive_event_test.rb
@@ -19,4 +19,16 @@ class ReceiveEventTest < ActionDispatch::IntegrationTest
     result = Chatwoot::ReceiveEvent.call(event: {})
     assert_equal true, result.failure?
   end
+
+  test "botpress two mesasges" do
+    stub_request(:post, Regexp.new(ENV['BOTPRESS_ENDPOINT'])).
+    to_return(status: 200, body: '{"responses":[{"type":"text","workflow":{},"text":"Testing 1","markdown":true,"typing":true},{"type":"text","workflow":{},"text":"Testing 2","markdown":true,"typing":true}]}', headers: {'Content-Type': 'application/json; charset=utf-8'})
+    stub_request(:post, Regexp.new(ENV['CHATWOOT_ENDPOINT'])).
+    to_return(status: 200, body: '{"id":65303,"content":"Testing 1","inbox_id":10,"conversation_id":11791,"message_type":1,"content_type":"text","content_attributes":{},"created_at":1656366978,"private":false,"source_id":null,"sender":{"id":3,"name":"Botpress Testing","avatar_url":"","type":"agent_bot"}}', headers: {'Content-Type': 'application/json; charset=utf-8'})
+    stub_request(:post, Regexp.new(ENV['CHATWOOT_ENDPOINT'])).
+    to_return(status: 200, body: '{"id":65304,"content":"Testing 2","inbox_id":10,"conversation_id":11791,"message_type":1,"content_type":"text","content_attributes":{},"created_at":1656366979,"private":false,"source_id":null,"sender":{"id":3,"name":"Botpress Testing","avatar_url":"","type":"agent_bot"}}', headers: {'Content-Type': 'application/json; charset=utf-8'})
+
+    result = Chatwoot::ReceiveEvent.call(event: @event)
+    assert_equal true, result.success?
+  end
 end
