6388233d90e2986a1bf97a792048f335055a70a2
Douglas Lara <douglasdelaras@gmail.com>
2022-07-15 15:08:39 -0300

    Implement feature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,19 @@ require_relative "../config/environment"
 require "rails/test_help"
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each { |rb| require(rb) }
 require 'webmock/minitest'
+require 'minitest/mock'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+
+  def mock_env(partial_env_hash)
+    old = ENV.to_hash
+    ENV.update partial_env_hash
+    begin
+      yield
+    ensure
+      ENV.replace old
+    end
+  end
 end
