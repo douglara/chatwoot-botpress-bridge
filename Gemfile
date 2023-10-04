cc0c70f2e7b05c70930585ed64182cfbc79116d5
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-10-04 12:23:17 -0300

    feat(SendToChatwoot): Add support to file sending
    - Implement file sending logic
    	- When botpress_response type is 'file' or 'image' or 'video'
    	- Downloads the file from the url in disk
    	- And then sends it to chatwoot
    		- Enable chatwoot file type identification by mime type
    			- Using the MimeMagic gem and the file_path
    
    - Create temp_files folder
    	- To store the files downloaded from the url
    
    - Create functions:
    	- response_image?
    	- response_file?
    	- response_video?
    	- download_file
    
    - Rename functions:
    	- 'botpress_response_choise_options?' -> 'response_choice_options?'
    		- Also correcting typo in the name
    	- build_choise_options_body -> build_choice_options_body
    		- Only correcting typo in the name
    
    - Add mimemagic dependency
    - Add byebug as a development/test dependency
    
    - Add some files/folders in .gitignore
    	- files in temp_files folder
    	- .byebug_history file
    	- .idea folder

--- Gemfile
+++ Gemfile
@@ -32,12 +32,15 @@ gem "bootsnap", require: false
 gem 'faraday'
 gem 'u-case', '~> 4.5.1'
 
+gem 'mimemagic'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'dotenv-rails'
   gem 'vcr'
   gem 'webmock'
+  gem 'byebug'
 end
 
 group :development do
--- app/use_cases/chatwoot/send_to_chatwoot.rb
+++ app/use_cases/chatwoot/send_to_chatwoot.rb
@@ -1,4 +1,8 @@
 require 'faraday'
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'mimemagic'
 
 class Chatwoot::SendToChatwoot < Micro::Case
   attributes :event
@@ -10,26 +14,96 @@ class Chatwoot::SendToChatwoot < Micro::Case
     chatwoot_endpoint = event['chatwoot_endpoint'] || ENV['CHATWOOT_ENDPOINT']
     chatwoot_bot_token = event['chatwoot_bot_token'] || ENV['CHATWOOT_BOT_TOKEN']
 
-    if botpress_response_choise_options?(botpress_response)
-      return Chatwoot::SendToChatwootRequest.call(
-        account_id: account_id, conversation_id: conversation_id, 
+    if response_image?(botpress_response) || response_file?(botpress_response) || response_video?(botpress_response)
+      request_url = "#{chatwoot_endpoint}/api/v1/accounts/#{account_id}/conversations/#{conversation_id}/messages"
+
+      file_url = botpress_response['image'] || botpress_response['file'] || botpress_response['video']
+      incoming_file_name = File.basename(file_url)
+      real_file_name = incoming_file_name.sub(/^[^-]+-/, '')
+      file_path = "temp_files/#{real_file_name}"
+      download_file(file_url, file_path)
+
+      url = URI(request_url)
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new(url)
+      request['api_access_token'] = chatwoot_bot_token
+
+      file = File.open(file_path)
+      file_mime_type = MimeMagic.by_path(file_path)
+
+      form_data = [
+        ['attachments[]', file, { content_type: file_mime_type }],
+        ['content', botpress_response['title']]
+      ]
+
+      request.set_form(form_data, 'multipart/form-data')
+      response = http.request(request)
+
+      if response.code == 200
+        Success result: JSON.parse(response.body)
+      elsif response.code == 404 && response.body.include?('Resource could not be found')
+        Failure result: { message: 'Chatwoot resource could not be found' }
+      elsif response.code == 404
+        Failure result: { message: 'Invalid chatwoot endpoint' }
+      elsif response.code == 401
+        Failure result: { message: 'Invalid chatwoot access token' }
+      else
+        Failure result: { message: 'Chatwoot server error' }
+      end
+    elsif response_choice_options?(botpress_response)
+      Chatwoot::SendToChatwootRequest.call(
+        account_id: account_id, conversation_id: conversation_id,
         chatwoot_endpoint: chatwoot_endpoint, chatwoot_bot_token: chatwoot_bot_token,
-        body: build_choise_options_body(botpress_response)
+        body: build_choice_options_body(botpress_response)
       )
     else
-      return Chatwoot::SendToChatwootRequest.call(
-        account_id: account_id, conversation_id: conversation_id, 
+      Chatwoot::SendToChatwootRequest.call(
+        account_id: account_id, conversation_id: conversation_id,
         chatwoot_endpoint: chatwoot_endpoint, chatwoot_bot_token: chatwoot_bot_token,
         body: { content: botpress_response['text'] }
       )
     end
   end
 
-  def botpress_response_choise_options?(botpress_response)
-    botpress_response['type'] == 'single-choice'
+  def response_image?(response)
+    response['type'] == 'image'
   end
 
-  def build_choise_options_body(botpress_response)
-    { content: botpress_response['text'], content_type: 'input_select', content_attributes: { items: botpress_response['choices'].map { | option | { title: option['title'], value:  option['value'] } } }  }
+  def response_file?(response)
+    response['type'] == 'file'
+  end
+
+  def response_video?(response)
+    response['type'] == 'video'
+  end
+
+  def response_choice_options?(response)
+    response['type'] == 'single-choice'
+  end
+
+  def build_choice_options_body(botpress_response)
+    {
+      content: botpress_response['text'],
+      content_type: 'input_select',
+      content_attributes: {
+        items: botpress_response['choices'].map {
+          | option |
+            {
+              title: option['title'],
+              value:  option['value']
+            }
+        }
+      }
+    }
+  end
+
+  def download_file(file_url, file_path)
+    file = Faraday.get(file_url).body
+
+    File.write(file_path, file, mode: 'wb', encoding: 'ASCII-8BIT')
   end
 end
