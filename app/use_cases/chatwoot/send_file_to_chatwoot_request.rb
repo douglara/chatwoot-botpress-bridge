f5f11f83cdf1d4a9cf7f3f2c88aeac219c149ed4
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2023-10-11 10:34:52 -0300

    test(SendFileToChatwootRequest): Add tests for SendFileToChatwootRequest
    - Create SendFileToChatwootRequestTest:
      - Test SendFileToChatwootRequest get_file_name_by_url function:
        - 'should return valid file name'
        - 'should throw invalid file name error'
    
    - Add tests in ChatwootControllerTest:
      - 'should send image'
      - 'should send video'
      - 'should send file'
      - 'should send file with message'
    
    - In SendFileToChatwootRequest:
      - Make the form_data content optional
      - Fix use case response by transforming the status code into string
      - Add a try...catch to download_file function
      - Add in get_file_name_by_url function a validation
        - If not valid throw an error
    
    - In ReceiveEvent:
      - Capture each of the chatwoot responses
        - To return at use case response
    
    - Add test files in fixtures:
      - image-file.png
      - pdf-file.pdf
      - video.file.mp4

--- app/use_cases/chatwoot/send_file_to_chatwoot_request.rb
+++ app/use_cases/chatwoot/send_file_to_chatwoot_request.rb
@@ -25,10 +25,11 @@ class Chatwoot::SendFileToChatwootRequest < Micro::Case
     request = Net::HTTP::Post.new(url)
     request['api_access_token'] = chatwoot_bot_token
 
-    form_data = [
-      ['content', botpress_response['title']],
-      ['attachments[]', file, { filename: file_name , content_type: file.content_type }]
-    ]
+    form_data = [['attachments[]', file, { filename: file_name , content_type: file.content_type }]]
+
+    unless botpress_response['title'].nil?
+      form_data << ['content', botpress_response['title']]
+    end
 
     request.set_form(form_data, 'multipart/form-data')
     response = http.request(request)
@@ -37,13 +38,13 @@ class Chatwoot::SendFileToChatwootRequest < Micro::Case
     Rails.logger.info("Status code: #{response.code}")
     Rails.logger.info("Body: #{response.body}")
 
-    if response.code == 200
+    if response.code == "200"
       Success result: JSON.parse(response.body)
-    elsif response.code == 404 && response.body.include?('Resource could not be found')
+    elsif response.code == "404" && response.body.include?('Resource could not be found')
       Failure result: { message: 'Chatwoot resource could not be found' }
-    elsif response.code == 404
+    elsif response.code == "404"
       Failure result: { message: 'Invalid chatwoot endpoint' }
-    elsif response.code == 401
+    elsif response.code == "401"
       Failure result: { message: 'Invalid chatwoot access token' }
     else
       Failure result: { message: 'Chatwoot server error' }
@@ -51,11 +52,16 @@ class Chatwoot::SendFileToChatwootRequest < Micro::Case
   end
 
   def download_file(file_url)
-    OpenURI::open_uri(file_url)
+    begin
+      OpenURI::open_uri(file_url)
+    rescue
+      raise "File url is invalid"
+    end
   end
 
   def get_file_name_by_url(file_url)
     incoming_file_name = File.basename(file_url)
+    raise "Invalid file name" unless incoming_file_name.include?('-')
     incoming_file_name.sub(/^[^-]+-/, '')
   end
 end
